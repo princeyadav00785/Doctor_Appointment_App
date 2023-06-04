a072ef92d515bde28a6884795c09b15181bc9aa5
princeyadav00785 <princeyadav00785@example.com>
2023-06-04 09:00:49 +0530

    Iniialiing project setup

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-dark">
+        <h1 className="text-success">HELLO</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
